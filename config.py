d3731e8336c7cdaf23a7389dd6b935cc054f83b5
Tshhdgu <104243541+Tshhdgu@users.noreply.github.com>
2024-06-15 16:17:11 +0530

    Update config.py

--- config.py
+++ config.py
@@ -3,22 +3,26 @@ id_pattern = re.compile(r'^.\d+$')
 
 class Config(object):
     # pyro client config
-    API_ID    = os.environ.get("API_ID", "27996421")
-    API_HASH  = os.environ.get("API_HASH", "5f5cb6a13ecff3d90e1ec73dc366e26d")
-    BOT_TOKEN = os.environ.get("BOT_TOKEN", "7061401463:AAHlTvU-VjY74Cm2obpoRH3Z3jzYrKQZWA4") 
+    # API_ID    = os.environ.get("API_ID", "27996421")
+    # API_HASH  = os.environ.get("API_HASH", "5f5cb6a13ecff3d90e1ec73dc366e26d")
+    # BOT_TOKEN = os.environ.get("BOT_TOKEN", "7304804258:AAGYCi_E2CpXdp3-fv951KFsTX2kyqptT6Y")
+    API_ID    = os.environ.get("API_ID", "26184984")
+    API_HASH  = os.environ.get("API_HASH", "d89d7c5ac61ea8f467a6a75aa54a51ca")
+    BOT_TOKEN = os.environ.get("BOT_TOKEN", "6087816605:AAFsPV-uIOs8OhTSF9PN2lOE2GbdAzMTep8")
+
 
     # database config
     DB_NAME = os.environ.get("DB_NAME","rename")     
-    DB_URL  = os.environ.get("DB_URL","mongodb+srv://cijiy83739:UNR4AkUEZNg9a0jL@cluster0.u8iw9we.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-    # DB_URL  = os.environ.get("mongodb+srv://beyekec464:NpeHanfZJwDz1YGE@cluster0.cafwct1.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
-     # mongodb+srv://codiyoc903:R8JnDcnrsRh6wy3D@cluster0.z1avpag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0
-
+    # DB_URL  = os.environ.get("DB_URL","mongodb+srv://codiyoc903:R8JnDcnrsRh6wy3D@cluster0.z1avpag.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+    DB_URL  = os.environ.get("DB_URL","mongodb+srv://dapita1597:0irZBo8nAyTn9pdu@cluster0.zurtp5f.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0")
+ 
     # other configs
     BOT_UPTIME  = time.time()
     START_PIC   = os.environ.get("START_PIC", "https://telegra.ph/file/0737a4e61db2065771a35.jpg")
-    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5050376364 6887525311').split()]
-    FORCE_SUB   = os.environ.get("FORCE_SUB", "enewj") 
-    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002123581785"))
+    ADMIN       = [int(admin) if id_pattern.search(admin) else admin for admin in os.environ.get('ADMIN', '5050376364 6887525311 6506664860').split()]
+    FORCE_SUB   = os.environ.get("FORCE_SUB", "cine_huntsupdates") 
+    # LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002123581785"))
+    LOG_CHANNEL = int(os.environ.get("LOG_CHANNEL", "-1002146552264"))
     
     # wes response configuration     
     WEBHOOK = bool(os.environ.get("WEBHOOK", "True"))
@@ -37,7 +41,7 @@ class Txt(object):
     
 ➻ Use /tutorial Command To Know How To Use Me.
 
-<b>Bot Is Made By @rohankaranje</b>
+<b>Bot Is Made By @rohankaranjep</b>
 """
     
     FILE_NAME_TXT = """<b><u>SETUP AUTO RENAME FORMAT</u></b>
@@ -58,7 +62,7 @@ Use These Keywords To Setup Custom File Name
 <b>📢 Channel :</b> <a href=''>Cinehunts</a>
 <b>🧑‍💻 Developer :</b> <a href='https://t.me/rohankaranje'>rohankaranje</a>
     
-<b>♻️ Bot Made By :</b> @rohankaranje"""
+<b>♻️ Bot Made By :</b> @PandaWep"""
 
     
     THUMBNAIL_TXT = """<b><u>🖼️  HOW TO SET THUMBNAIL</u></b>
